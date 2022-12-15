14017310b5423231d8e80b3536376d77d0819ff7
Aster <galaster@foxmail.com>
2022-12-15 17:42:54 +0800

    🔮 Define the bottle neck block

---
+++ projects/deep-danbooru/src/models/bottleneck.rs
@@ -0,0 +1,24 @@
+use super::*;
+
+impl Module for Bottleneck {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let ls = xs.apply(&self.conv1)
+            .relu()
+            .apply(&self.conv2)
+            .relu()
+            .apply(&self.conv3);
+        xs.apply(&self.conv4).add(ls).relu()
+    }
+}
+
+impl Bottleneck {
+    pub fn new(vs: &tch::nn::Path, in_channels: i64, out_channels: i64) -> Self {
+        let mut config = ConvConfig::default();
+        config.padding = 1;
+        let conv1 = conv2d(vs / "conv1", in_channels, out_channels / 4, 1, Default::default());
+        let conv2 = conv2d(vs / "conv2", out_channels / 4, out_channels / 4, 3, config);
+        let conv3 = conv2d(vs / "conv3", out_channels / 4, out_channels, 1, Default::default());
+        let conv4 = conv2d(vs / "conv4", in_channels, out_channels, 1, Default::default());
+        Self { conv1, conv2, conv3, conv4 }
+    }
+}
--- projects/deep-danbooru/src/models/mod.rs
+++ projects/deep-danbooru/src/models/mod.rs
@@ -1,29 +1,30 @@
-use tch::nn::Module;
+use std::ops::Add;
+
+use tch::nn::{Conv2D, conv2d, ConvConfig, Module};
 use tch::Tensor;
 
 mod head;
 mod basic;
+mod bottleneck;
 
 #[derive(Debug)]
 pub struct HeadBlock {
-
+    conv: Conv2D,
 }
 
 #[derive(Debug)]
-pub struct BasicBlock {}
-
-#[derive(Debug)]
-pub struct Bottleneck {}
-
-impl Module for BasicBlock {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        todo!()
-    }
+pub struct BasicBlock {
+    conv1: Conv2D,
+    conv2: Conv2D,
+    conv3: Conv2D,
 }
 
 
-impl Module for Bottleneck {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        todo!()
-    }
+
+#[derive(Debug)]
+pub struct Bottleneck {
+    conv1: Conv2D,
+    conv2: Conv2D,
+    conv3: Conv2D,
+    conv4: Conv2D,
 }
